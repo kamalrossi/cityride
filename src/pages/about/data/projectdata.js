8d62db3a9dd075e0a5d3aabdf75dee75d2c2e67d
kamalrossi <rossikamal@gmail.com>
2022-06-30 19:59:59 +0600

    first commit

---
+++ src/pages/about/data/projectdata.js
@@ -0,0 +1,47 @@
+const projectowner = "Rossi Kamal(rossikamal@gmail.com)";
+const projectname = "CityRide";
+const projectdesc = {
+  title: "Title",
+  desc:
+    "CityRide is a  dashboard for ride-sharing app with modules namely rider, driver, ride, cancellation and analytics."
+};
+const projecttag = [
+  {
+    tag1: "ReactJS",
+    tag2: "CSS",
+    tag3: "HTML",
+    tag4: "IT"
+  }
+];
+const projecttime = [
+  {
+    title: "UX/UI  Design",
+    from: "Apr 22",
+    to: "Jun 22"
+  },
+  {
+    title: "Responsive Web Development",
+    from: "Apr 22",
+    to: "Jun 22"
+  }
+];
+
+const projectrole = [
+  {
+    title: "UX Design",
+    desc: " UX design of simple and responsive admin dashboard"
+  },
+  {
+    title: "Responsive Landing Page Development",
+    desc: " Development of  responsive website by integrating Chart and Graph APIs for data visualization"
+  }
+];
+
+export {
+  projectowner,
+  projectname,
+  projectdesc,
+  projectrole,
+  projecttime,
+  projecttag
+};
